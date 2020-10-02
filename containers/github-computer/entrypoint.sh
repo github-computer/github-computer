e3c5a26b7279955eab9ecfa5bb6d95c263f24e3b
Jackson Delahunt <jackson@stemn.com>
2020-10-02 17:16:48 +1000

    init

---
+++ containers/github-computer/entrypoint.sh
@@ -0,0 +1,8 @@
+# Start services
+~/.config/scripts/startup.sh
+
+# Start vnc access
+~/.config/scripts/vnc.sh
+
+# Start localtunnel agent
+~/.yarn/bin/lt --host http://github-computer.app --print-requests --port 80
